9bee4a98c7df4f37a2bbd6b5889ac80b89778e51
xiranou <xiranou@gmail.com>
2017-03-11 12:40:51 -0500

    update home button

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import App from './App';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showSession: false
+    };
+
+    this.displaySession = this.displaySession.bind(this);
+  }
+
+  displaySession() {
+    this.setState({
+      showSession: true
+    });
+  }
+
   render() {
     return (
-      <Link to='/app'>Join</Link>
+      <div>
+        <button onClick={this.displaySession}>Join</button>
+        {
+          this.state.showSession ? <App /> : null
+        }
+      </div>
     );
   }
 }
