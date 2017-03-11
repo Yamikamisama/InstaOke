1835c36b90b5a12ed42008678703912601b8eb04
xiranou <xiranou@gmail.com>
2017-03-11 12:21:45 -0500

    add Home

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Router, Route } from 'react-router';
 
 import App from './App';
+import Home from './Home';
 import About from './About';
 
 const Routes = (props) => (
   <Router {...props}>
     <div>
-      <Route exact path="/" component={App} />
+      <Route exact path="/" component={Home} />
+      <Route path="/app" component={App}></Route>
       <Route path="/about" component={About} />
     </div>
   </Router>
